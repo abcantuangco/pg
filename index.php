403d4d966ebf1547e902dd7dd1d135abf3342baa
ABCantuangco <abcantuangco@gmail.com>
2014-03-05 18:42:40 +0800

    add initial html code for the form

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Answer 1.1</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <table>
+        <tr>
+            <td width="50%">Enter a positive integer:</td>
+            <td width="50%"><input type="text" name="intvalue"/></td>
+        </tr>
+    </table>
+    
+</body>
+</html>
